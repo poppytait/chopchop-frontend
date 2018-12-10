b9b672ed6907728761324e1f52d5b7ea3a173020
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2018-12-10 10:56:34 +0100

    add env

--- src/lib/auth-service.js
+++ src/lib/auth-service.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 class Auth {
   constructor() {
     this.auth = axios.create({
-      baseURL: 'http://localhost:5000',
+      baseURL: `${process.env.REACT_APP_API_URL}`,
       withCredentials: true
-    })
+    });
   }
 
   signup(user) {
